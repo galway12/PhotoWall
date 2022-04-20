78bcd8b9e4b270d496842a031e678820a544ac54
galway12 <2239938989@qq.com>
2022-04-20 10:51:20 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const {
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base:'./',
+	base:'/PhotoWall/dist',
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
