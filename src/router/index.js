fbae6994d3df4015f0ef47c473ece20a7e4da4de
galway12 <2239938989@qq.com>
2022-04-20 08:29:22 +0800

    123

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 // 引入 router
 import {
 	createRouter,
-	createWebHistory
+	createWebHashHistory
 } from 'vue-router'
 // 引入路由各页面配置
 import routes from './routes'
 // 配置router对象
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes
 })
 export default router
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const {
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	base:'./',
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
